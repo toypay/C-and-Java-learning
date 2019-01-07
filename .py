5934cd7a6bbb521596e8429145f93a68591fc6b4
toypay <46281507+toypay@users.noreply.github.com>
2019-01-07 14:11:24 +0800

    Update .py

--- .py
+++ .py
@@ -15,3 +15,12 @@ print(世界最可愛)
 youtube="subs to pewdiepie"
 print(youtube)
 print("\tfairy\n\tyellow\n\tice")
+name=input("what is your name? :")
+gender=input("what is your gender? :")
+print("information","\n\t",name,"\n\t",gender)
+j=True
+p=False
+print(j+p)
+print(int(3.5))
+print(float(3.5))
+print(float(int(3.5)))
